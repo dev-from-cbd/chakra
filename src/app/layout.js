9961b8a1466cdf2a4a83a42d4a03774b4de6f0b4
dev-from-cbd <veryfatwombat@gmail.com>
2023-10-26 06:17:37 +1030

    Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,21 @@
+// Import global CSS styles for the application.
 import "./globals.css";
+// Import the 'Inter' font from Google Fonts using the 'next/font' package.
 import { Inter } from "next/font/google";
 
+// Define a variable 'inter' to load the 'Inter' font with a Latin character subset.
 const inter = Inter({ subsets: ["latin"] });
 
+// Define metadata for the application, including the title and description.
 export const metadata = {
   title: "Chakra UI, React, Github API",
   description: "GitHub User Finder",
 };
 
+// Define the 'RootLayout' component which receives 'children' as props.
 export default function RootLayout({ children }) {
   return (
+    // Start an HTML document with the language set to English.
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
