724e0f1ff7858c13305c85575ee1c7305c272baa
dev-from-cbd <veryfatwombat@gmail.com>
2023-10-27 19:47:35 +1030

    Comments

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
+// Import the 'Image' component from the 'next/image' module
 import Image from "next/image";
+
+// Import styles from a local CSS module named 'page.module.css'
 import styles from "./page.module.css";
 
+// Define the default function 'Home' for the home page
 export default function Home() {
+  // Return the main content with the text 'Main'
   return <main>Main</main>;
 }
